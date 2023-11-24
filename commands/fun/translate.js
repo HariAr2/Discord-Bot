ec2eee45b818ea858b698cb30e6b409ff047062b
Hari Aravindh <127916422+HariAr2@users.noreply.github.com>
2023-11-24 20:55:02 +0530

    Update translate.js

--- commands/fun/translate.js
+++ commands/fun/translate.js
@@ -16,7 +16,7 @@ module.exports = {
     if (!query) return await interaction.reply('Please enter a word to translate');
 
     try {
-      const translated = await translate(query, { from:'en', to: 'ko' });//language code that uses ISO 639-1 so 'ko'-korean 'en'-english 'ja'-japanese
+      const translated = await translate(query, { from:'en', to: 'ko' });//language code that uses ISO 639-1 so 'ko'-korean 'en'-english 'ja'-japanese(google ISO 639-1 codes for languages)
       await interaction.reply(translated.text);
     } catch (error) {
       console.error(error);
