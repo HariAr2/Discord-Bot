8aa2b30f7445d1a0980f3305d70b71aaafc727cd
Hari Aravindh <127916422+HariAr2@users.noreply.github.com>
2023-11-28 22:36:52 +0530

    Update user.js

--- commands/utility/user.js
+++ commands/utility/user.js
@@ -7,4 +7,4 @@ module.exports = {
 	async execute(interaction) {
 		await interaction.reply(`This command was run by ${interaction.user.username},who joined on ${interaction.member.joinedAt}.`);
 	},
-};
+};  
