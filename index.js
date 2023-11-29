f209ef92bc293285f7bae760d24ddc7fee377267
Hari Aravindh <127916422+HariAr2@users.noreply.github.com>
2023-11-29 23:38:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
+
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
